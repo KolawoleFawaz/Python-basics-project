87b9e00ac319b76de3ed92d13f611a2f8fc28329
Fawaz Azeez <49353217+aziz300@users.noreply.github.com>
2022-07-11 00:36:59 +0100

    Add files via upload

---
+++ integer_n.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+def sol(n):
+    solution = n + int(str(n)+str(n)) + int(str(n)+str(n)+str(n))
+    return solution
+sol(5)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
