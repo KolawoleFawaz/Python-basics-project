ad4b6835d0588effa724f30bc5ddf9b5211b8e73
Fawaz Azeez <49353217+aziz300@users.noreply.github.com>
2022-07-11 00:00:39 +0100

    Add files via upload

---
+++ names.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def names(firstname,lastname):
+    solution = lastname + " " + firstname
+    
+    return solution
+
+names("fawaz","azeez")
+
+
+# In[ ]:
+
+
+
+
