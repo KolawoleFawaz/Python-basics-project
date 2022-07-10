64438e3ed5852f7ccb6d82d9c7feeb79ed4be8aa
Fawaz Azeez <49353217+aziz300@users.noreply.github.com>
2022-07-11 00:56:06 +0100

    Add files via upload

---
+++ EvenAndOdd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+number = int(input("Input a number: "))
+
+if number % 2 == 0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
+
+
+# In[ ]:
+
+
+
+
